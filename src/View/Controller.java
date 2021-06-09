bccbea4ad311b7c0bfd7895c4705440707a1ede2
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-09 20:29:20 +0300

    Add files via upload

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -1,247 +1,203 @@
 package View;
 
-import Model.Model;
+import View.Graphs.Graphs;
+import View.Icons.Icons;
+import View.Joystick.Joystick;
+import View.ListView.ListView;
+import View.OpenFiles.OpenFiles;
 import ViewModel.viewModel;
-import XML.Item;
-import XML.StaXParser;
-import XML.StaxWriter;
-import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.control.*;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
 
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Controller implements Initializable {
 
-    Map <String, List<String>> listMap;
-    Model model = new Model();
-    //TimeSeries timeSeries = new Model.TimeSeries();
-    viewModel viewModel = new viewModel(model);
-    DoubleProperty aileron, elevators;
+    viewModel vm;
 
-    @FXML
-    public Button ReadAndWriteXML;
-    @FXML
-    public Pane paneParent;
-    @FXML
-    public Pane attributesTableParent;
-    @FXML
-    public Slider sliderControllerSpeed;
-    @FXML
-    private ChoiceBox choiceBox;
-    @FXML
-    private ChoiceBox choiceBox2;
-    @FXML
-    public Pane joystickSliderPane;
-    @FXML
-    private Slider throttle;
-    @FXML
-    private Slider rudder;
-    @FXML
-    public Canvas joystick;
-    @FXML
-    public ListView attributes;
-    @FXML
-    public Label labelSpeed;
-    @FXML
-    public ImageView play;
-    @FXML
-    public ImageView pause;
-    @FXML
-    public ImageView stop;
-    @FXML
-    public ImageView speed;
-    @FXML
-    public ImageView slow;
-    @FXML
-    public ImageView doubleSpeedUp;
-    @FXML
-    public ImageView doubleSpeedDown;
-
-    public Controller() { }
-
-    private boolean atEndOfVideo = false;
-    private boolean isPlaying = true;
+    @FXML Joystick MyJoystick;
+    @FXML ListView MyListView;
+    @FXML Graphs MyGraph;
+    @FXML Icons myIcons;
+    @FXML OpenFiles MyOpenFiles;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        ObservableList<String> attributes1 = FXCollections.observableArrayList("airspeed-indicator", "altimeter_pressure",
-                "attitude-pitch_deg", "attitude-roll_deg", "attitude-pitch_deg", "attitude-roll_deg", "gps_altitude", "magnetic-compass",
-                "skid-ball", "vertical-speed");
-        choiceBox.setItems(attributes1);
-
-        ObservableList<String> CSVFiles = FXCollections.observableArrayList("anomaly_flight.csv", "reg_flight.csv");
-        choiceBox2.setItems(CSVFiles);
-
-        viewModel.rudder.bind(rudder.valueProperty());
-        viewModel.throttle.bind(throttle.valueProperty());
-        aileron = new SimpleDoubleProperty();
-        elevators = new SimpleDoubleProperty();
-        viewModel.aileron.bind(aileron);
-        viewModel.elevators.bind(elevators);
-
-        /*  MediaView mediaView = new MediaView(new File(""));
-        **********  sliderControllerSpeed.setAccessibleText("1X"); *************
-
-        labelSpeed.setOnMouseClicked(mouseEvent -> {
-            if(labelSpeed.getText().equals("1X")) {
-                labelSpeed.setText("2X");
-                //the video
-                //mpVideo.setRate(2.0);
-            }
-            else
-            {
-                labelSpeed.setText("1X");
-                //mpVideo.setRate(1X);
-            }
-        });
-
-        sliderControllerSpeed.valueChangingProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean wasChanging, Boolean isChanging) {
-                if(!isChanging)
-                {
-                    //mpVideo.seek(Duration.seconds(sliderControllerSpeed.getValue());//the slider working at seconds.
-                }
-            }
-        });
-       sliderControllerSpeed.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
-                double currentTime = mpVideo.getCurrentTime().toSeconds();
-                if(Math.abs(currentTime - newValue.doubleValue()) > 0.5){
-                    //mpVideo.seek(Duration.seconds(newValue.newValue.doubleValue()))
-                }
-            }
-        });
-
-        bindCorrectTimeLabel();
-    }
-
-
-    public void bindCorrectTimeLabel(){
-        labelCurrentTime.textProperty().bind(Binding.createStringBinding(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                return getTime(myVideo.getCurrentTime()) + " / ";
-            }
-        }, mpVideo.CurrentTimeProperty()));
-
-    }
-    public String getTime(Duration time)
-    {
-        int hours = (int)time.toHours();
-        int minutes = (int)time.toMinutes();
-        int seconds = (int)time.toSeconds();
-
-        if(seconds > 59 ) seconds = seconds % 60;
-        if(minutes > 59 ) minutes = minutes % 60;
-        if(hours > 59 ) hours = hours % 60;
-
-        if(hours > 0) return String.format("%d:%02d:%02d",hours,minutes,seconds);
-        else{
-            return String.format("%02d:%02d",minutes,seconds);
-        }
-    }
-*/
-    }
-
-    public void playClicked(MouseEvent mouseEvent){
-        System.out.println("play the simulator");
-    }
-
-    public void pauseClicked(MouseEvent mouseEvent){
-        System.out.println("pause the simulator");
-    }
-
-    public void stopClicked(MouseEvent mouseEvent){
-        System.out.println("stop the simulator");
-    }
-
-    public void speedDownClicked(MouseEvent mouseEvent){
-        System.out.println("slow down the simulator");
-    }
-
-    public void speedUpClicked(MouseEvent mouseEvent){
-        System.out.println("speed up the simulator");
-    }
-
-    public void doubleSpeedDownClicked(MouseEvent mouseEvent){
-        System.out.println("slow down X2 the simulator");
-    }
-
-    public void doubleSpeedUpClicked(MouseEvent mouseEvent){
-        System.out.println("speed up X2 the simulator");
-    }
-
-    public void WriteXML(MouseEvent mouseEvent){
-        System.out.println("XML file read and write...");
-        StaXParser read = new StaXParser();
-        StaxWriter write = new StaxWriter();
-        List<Item> readConfig = read.readConfig("playback_small.xml");
-        write.Writer(readConfig);
-    }
-
-    public void openCSVFile(MouseEvent mouseEvent) {
-        System.out.println("open csv file...");
-        if(choiceBox2.getSelectionModel().getSelectedItem().equals("anomaly_flight.csv")){
-            model.openCSVFile("anomaly_flight.csv");
-            System.out.println("anomaly_flight opened");
-        } else {
-            model.openCSVFile("reg_flight.csv");
-            System.out.println("reg_flight opened");
-        }
-        listMap = model.getMap();
-        model.setAnomalyDetector(model.setTimeSeries("anomaly_flight.csv"));
-        System.out.println("Done");
-    }
-
-    public void AttributesChoose(MouseEvent mouseEvent){
-        switch (choiceBox.getSelectionModel().getSelectedIndex()){
-            case 0:
-                listMap.get("airspeed-indicator_indicated-speed-kt");
-                //map.paint(airspeed);
-                break;
-            case 1:
-                listMap.get("altimeter_pressure-alt-ft");
-                break;
-            case 2:
-                listMap.get("attitude-indicator_indicated-pitch-deg");
-                break;
-            case 3:
-                listMap.get("attitude-indicator_indicated-roll-deg");
-                break;
-            case 4:
-                listMap.get("attitude-indicator_internal-pitch-deg");
-                break;
-            case 5:
-                listMap.get("attitude-indicator_internal-roll-deg");
-                break;
-            case 6:
-                listMap.get("gps_indicated-altitude-ft");
-                break;
-            case 7:
-                listMap.get("magnetic-compass_indicated-heading-deg");
-                break;
-            case 8:
-                listMap.get("slip-skid-ball_indicated-slip-skid");
-                break;
-            case 9:
-                listMap.get("vertical-speed-indicator_indicated-speed-fpm");
-                break;
-
-        }
+    public void initialize(URL location, ResourceBundle resources) {
+        MyJoystick.aileron = new SimpleDoubleProperty();
+        MyJoystick.elevators = new SimpleDoubleProperty();
+        MyJoystick.rudder = new SimpleDoubleProperty();
+        MyJoystick.throttle = new SimpleDoubleProperty();
     }
 
 }
+
+//    public Runnable onPlay, onPause, onStop;
+//
+//    @FXML public Pane paneParent;
+//    @FXML public Slider sliderControllerSpeed;
+//    @FXML private ChoiceBox choiceBox2;
+//    @FXML private ChoiceBox choiceBox;
+//    @FXML public Button ReadAndWriteXML;
+//    @FXML private Slider throttle;
+//    @FXML private Slider rudder;
+//    @FXML View.Icons.Icons Icons;
+//
+//    @FXML public Pane IconsPane;
+//    @FXML public Label labelSpeed;
+//    @FXML public ImageView play;
+//    @FXML public ImageView pause;
+//    @FXML public ImageView stop;
+//    @FXML public ImageView speed;
+//    @FXML public ImageView slow;
+//    @FXML public ImageView doubleSpeedUp;
+//    @FXML public ImageView doubleSpeedDown;
+//
+//    Map<String, List<String>> listMap;
+//    Model model = new Model();
+//    ViewModel.viewModel viewModel = new viewModel(model);
+//    DoubleProperty aileron, elevators;
+//
+//    public void init() {
+//        ObservableList<String> attributes1 = FXCollections.observableArrayList("airspeed-indicator", "altimeter_pressure",
+//                "attitude-pitch_deg", "attitude-roll_deg", "attitude-pitch_deg", "attitude-roll_deg", "gps_altitude", "magnetic-compass",
+//                "skid-ball", "vertical-speed");
+//        choiceBox.setItems(attributes1);
+//
+//        ObservableList<String> CSVFiles = FXCollections.observableArrayList("anomaly_flight.csv", "reg_flight.csv");
+//        choiceBox2.setItems(CSVFiles);
+//
+//        if(this.rudder!=null && this.throttle!=null) {
+//            viewModel.rudder.bind(rudder.valueProperty());
+//            viewModel.throttle.bind(throttle.valueProperty());
+//            aileron = new SimpleDoubleProperty();
+//            elevators = new SimpleDoubleProperty();
+//            viewModel.aileron.bind(aileron);
+//            viewModel.elevators.bind(elevators);
+//
+//            viewModel vm = new viewModel();
+//           // Icons.controller.onPlay=vm.play;
+//           // Icons.controller.onPause=vm.pause;
+//           // Icons.controller.onStop=vm.stop;
+//
+//            doubleSpeedDown = new ImageView();
+//            slow = new ImageView();
+//            play = new ImageView();
+//            pause = new ImageView();
+//            stop = new ImageView();
+//            speed = new ImageView();
+//            doubleSpeedUp = new ImageView();
+//
+//            doubleSpeedDown.setImage(new Image("speed_down.jpg"));
+//            slow.setImage(new Image("slow.jpg"));
+//            play.setImage(new Image("play.jpg"));
+//            pause.setImage(new Image("pause.jpg"));
+//            stop.setImage(new Image("stop.jpg"));
+//            speed.setImage(new Image("speed.jpg"));
+//            doubleSpeedUp.setImage(new Image("speed_up.jpg"));
+//
+//
+//        }
+//    }
+//
+//
+//    public void WriteXML(MouseEvent mouseEvent){
+//        System.out.println("XML file read and write...");
+//        StaXParser read = new StaXParser();
+//        StaxWriter write = new StaxWriter();
+//        List<Item> readConfig = read.readConfig("playback_small.xml");
+//        write.Writer(readConfig);
+//    }
+//
+//    public void openCSVFile(MouseEvent mouseEvent) {
+//        System.out.println("open csv file...");
+//        if(choiceBox2.getSelectionModel().getSelectedItem().equals("anomaly_flight.csv")){
+//            model.openCSVFile("anomaly_flight.csv");
+//            System.out.println("anomaly_flight opened");
+//        } else {
+//            model.openCSVFile("reg_flight.csv");
+//            System.out.println("reg_flight opened");
+//        }
+//        listMap = model.getMap();
+//        model.setAnomalyDetector(model.setTimeSeries("anomaly_flight.csv"));
+//        System.out.println("Done");
+//    }
+//
+//    public void AttributesChoose(MouseEvent mouseEvent){
+//        switch (choiceBox.getSelectionModel().getSelectedIndex()){
+//            case 0:
+//                listMap.get("airspeed-indicator_indicated-speed-kt");
+//                //map.paint(airspeed);
+//                break;
+//            case 1:
+//                listMap.get("altimeter_pressure-alt-ft");
+//                break;
+//            case 2:
+//                listMap.get("attitude-indicator_indicated-pitch-deg");
+//                break;
+//            case 3:
+//                listMap.get("attitude-indicator_indicated-roll-deg");
+//                break;
+//            case 4:
+//                listMap.get("attitude-indicator_internal-pitch-deg");
+//                break;
+//            case 5:
+//                listMap.get("attitude-indicator_internal-roll-deg");
+//                break;
+//            case 6:
+//                listMap.get("gps_indicated-altitude-ft");
+//                break;
+//            case 7:
+//                listMap.get("magnetic-compass_indicated-heading-deg");
+//                break;
+//            case 8:
+//                listMap.get("slip-skid-ball_indicated-slip-skid");
+//                break;
+//            case 9:
+//                listMap.get("vertical-speed-indicator_indicated-speed-fpm");
+//                break;
+//
+//        }
+//    }
+//
+//
+//    public void playClicked(MouseEvent mouseEvent){
+//        System.out.println("play the simulator");
+//    }
+//    public void pauseClicked(MouseEvent mouseEvent){
+//        System.out.println("pause the simulator");
+//    }
+//    public void stopClicked(MouseEvent mouseEvent){
+//        System.out.println("stop the simulator");
+//    }
+//    public void speedDownClicked(MouseEvent mouseEvent){
+//        System.out.println("slow down the simulator");
+//    }
+//    public void speedUpClicked(MouseEvent mouseEvent){
+//        System.out.println("speed up the simulator");
+//    }
+//    public void doubleSpeedDownClicked(MouseEvent mouseEvent){
+//        System.out.println("slow down X2 the simulator");
+//    }
+//    public void doubleSpeedUpClicked(MouseEvent mouseEvent){
+//        System.out.println("speed up X2 the simulator");
+//    }
+//
+//
+//    public Controller(){}
+//
+//    private boolean atEndOfVideo = false;
+//    private boolean isPlaying = true;
+//
+//
+//
+//
+//
+//    @Override
+//    public void update(Observable o, Object arg) {
+//
+//    }
+//
+//
+//}
--- src/View/Main.java
+++ src/View/Main.java
@@ -1,29 +1,44 @@
 package View;
 
 import Model.Model;
+import Model.SimulatorClient;
 import ViewModel.viewModel;
-import XML.Item;
-import XML.StaXParser;
-import XML.StaxWriter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.List;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+//        FXMLLoader fxmlLoader = new FXMLLoader();
+//        Parent root = fxmlLoader.load(getClass().getResource("FlightSimulator.fxml").openStream());
+//        primaryStage.setTitle("Flight Simulator");
+//        Controller c = fxmlLoader.getController();
+//        c.init();
+//        primaryStage.setScene(new Scene(root, 700, 420));
+//        primaryStage.show();
+//        Model model = new Model();
+//        viewModel viewModel = new viewModel(model);
+//        model.addObserver(viewModel);
+//        Controller controller = fxmlLoader.getController();
+//        //viewModel.addObserver(controller);
+//        String ip = "127.0.0.1";
+//        int port = 5402;
+//        SimulatorClient simulatorClient = new SimulatorClient();
+//        //simulatorClient.Connect(ip,port);
+
         Parent root = FXMLLoader.load(getClass().getResource("FlightSimulator.fxml"));
         primaryStage.setTitle("Flight Simulator");
-        primaryStage.setScene(new Scene(root, 700, 420));
+        primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
-        Model model = new Model();
-        viewModel viewModel = new viewModel(model);
-
+        int port = 5402;
+        String ip = "127.0.0.1";
+        SimulatorClient simulatorClient = new SimulatorClient();
+       // simulatorClient.Connect(ip,port);
 
     }
 
