9047e8eecf1c01b9dfdea4b4894a89d703a44345
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-16 20:53:00 +0300

    Update CorrelatedFeatures.java

--- src/plugin/CorrelatedFeatures.java
+++ src/plugin/CorrelatedFeatures.java
@@ -2,15 +2,15 @@ package plugin;
 
 public class CorrelatedFeatures {
 	public final String feature1,feature2;
-	public final float corrlation;
+	public final float correlation;
 	public final Line lin_reg;
 	public final float threshold;
 	public final Circle c;
 	
-	public CorrelatedFeatures(String feature1, String feature2, float corrlation, Line lin_reg, float threshold, Circle c) {
+	public CorrelatedFeatures(String feature1, String feature2, float correlation, Line lin_reg, float threshold, Circle c) {
 		this.feature1 = feature1;
 		this.feature2 = feature2;
-		this.corrlation = corrlation;
+		this.correlation = correlation;
 		this.lin_reg = lin_reg;
 		this.threshold = threshold;
 		this.c = c;
