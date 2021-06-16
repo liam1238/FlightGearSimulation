af905b1ca2331e2090260944879ffe98ce0df71e
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-16 17:49:10 +0300

    Update JoystickController.java

--- src/View/Joystick/JoystickController.java
+++ src/View/Joystick/JoystickController.java
@@ -1,20 +1,14 @@
 package View.Joystick;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class JoystickController {
     @FXML Circle movingCircle;
-    @FXML Slider rudder;
-    @FXML Slider throttle;
+    @FXML Slider rudderSlider;
+    @FXML Slider throttleSlider;
     @FXML Label yawValue;
     @FXML Label AltitudeValue;
     @FXML Label DirectionValue;
@@ -25,17 +19,3 @@ public class JoystickController {
 
     public JoystickController() { }
 }
-
-
-
-//public class JoystickController implements Initializable {
-//
-//    @FXML public Pane joystickSliderPane;
-//    @FXML public Canvas joystick;
-//
-//
-//    @Override
-//    public void initialize(URL location, ResourceBundle resources) {
-//
-//    }
-//}
