2ec91f04bda5344faa736eaee603e8938da04add
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-16 17:42:46 +0300

    Update ModelInterface.java

--- src/Model/ModelInterface.java
+++ src/Model/ModelInterface.java
@@ -1,19 +1,13 @@
 package Model;
 
-import java.util.List;
-import java.util.Map;
-
 public interface ModelInterface {
-    public void openCSVFile(String propertiesFileName);
-    public TimeSeries setTimeSeries(String CSVFile);
-    public void setAnomalyDetector(TimeSeries timeSeries);
-    public void play (int start, int rate);
-    public void pause();
-    public void stop();
-    public void speedUp();
-    public void speedDown();
-    public void doubleSpeedUp();
-    public void doubleSpeedDown();
 
-    public Map<String, List<String>> getMap();
+    void openCSVFile();
+    void play ();
+    void pause();
+    void stop();
+    void speedUp();
+    void speedDown();
+    void doubleSpeedUp();
+    void doubleSpeedDown();
 }
