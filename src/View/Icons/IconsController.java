dbd2b78fbf4d45a1ea35a99ddc24e9e833b83d97
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-16 17:46:04 +0300

    Update IconsController.java

--- src/View/Icons/IconsController.java
+++ src/View/Icons/IconsController.java
@@ -1,150 +1,71 @@
 package View.Icons;
 
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import View.Main;
+import ViewModel.ViewModel;
+import application.Utils;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-
-import java.util.concurrent.TimeUnit;
+import java.text.DecimalFormat;
 
 public class IconsController {
-    @FXML Button doubleBack;
-    @FXML Button back;
-    @FXML Button play;
-    @FXML Button pause;
-    @FXML Button stop;
-    @FXML Button forward;
-    @FXML Button doubleForward;
-    @FXML ChoiceBox videoSpeed;
-    @FXML Slider videoSlider;
-    @FXML Label videoTime;
-    StringProperty FlightStatus;
-    DoubleProperty forwardCnt;
-    double f = 0;
-    SimpleDoubleProperty forward2Cnt;
-    SimpleDoubleProperty backwardCnt;
-    SimpleDoubleProperty backward2Cnt;
+    ViewModel vm;
+    @FXML Slider speedMultySlider;
+    @FXML Button superSlowButton;
+    @FXML Button slowButton;
+    @FXML Button playButton;
+    @FXML Button pauseButton;
+    @FXML Button stopButton;
+    @FXML Button fastButton;
+    @FXML Button superFastButton;
+    @FXML TextField speedMultyTextfield;
+    @FXML Slider currentFlightTimeSlider;
+    @FXML Label currentFlightTimeLabel;
 
     public IconsController() {
         super();
-        FlightStatus = new SimpleStringProperty();
-        forwardCnt = new SimpleDoubleProperty();
-        forward2Cnt = new SimpleDoubleProperty();
-        backwardCnt = new SimpleDoubleProperty();
-        backward2Cnt = new SimpleDoubleProperty();
+        vm = Main.viewModel;
     }
 
-    @FXML public void startFlight() {
-        FlightStatus.setValue("Fly");
+    public void onClickPlay() { vm.play(); }
+    public void onClickPause() { vm.pause(); }
+    public void onClickStop() { vm.stop(); }
+    public void onClickSlow() { vm.speedDown(); }
+    public void onClickSuperSlow() { vm.doubleSpeedDown(); }
+    public void onClickFast() { vm.speedUp(); }
+    public void onClickSuperFast() { vm.doubleSpeedUp(); }
+    public void onTextChangedMultField() { vm.change(); }
+
+    //change label when sliding, but only change flight time when we drop the slider
+    public void onMousePressedTimeSlider() { Main.isTimeSliding = true; }
+    public void onMouseMovedTimeSlider() {
+        ((Slider)Utils.getNodeByID("currentFlightTimeSlider")).valueProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (!Main.isTimeSliding)
+                return;
+            ((Label)Utils.getNodeByID("currentFlightTimeLabel")).setText(Utils.msToTimeString((long)((Slider)Utils.getNodeByID("currentFlightTimeSlider")).getValue())); //update label
+        });
     }
 
-    @FXML private void stopFlight() {
-        if (FlightStatus.getValue() == null || (!FlightStatus.getValue().equals("Fly") && !FlightStatus.getValue().equals("pause Fly") && !FlightStatus.getValue().equals("skip"))) {
-            Alert a = new Alert(Alert.AlertType.ERROR);
-            a.setHeaderText("Error on stop flight");
-            a.setContentText("there is no flight to stop");
-            a.showAndWait();
-        } else
-            FlightStatus.set("not Fly");
+    public void onMouseReleasedTimeSlider() {
+        Main.viewModel.simulatorApi.setCurrentFlightTime((int)((Slider)Utils.getNodeByID("currentFlightTimeSlider")).getValue()); //set flight time
+        Main.isTimeSliding = false;
     }
 
-    public String toStringTime(Double object) {
-        long seconds = object.longValue();
-        long minutes = TimeUnit.SECONDS.toMinutes(seconds);
-        long remainingSeconds = seconds - TimeUnit.MINUTES.toSeconds(minutes);
-        return String.format("%02d", minutes) + ":" + String.format("%02d", remainingSeconds);
+    //change label when sliding, but only change flight time when we drop the slider
+    public void onMousePressedSpeedSlider() {
+        Main.isSpeedSliding = true;
+    }
+    public void onMouseMovedSpeedSlider() {
+        ((Slider) Utils.getNodeByID("speedMultySlider")).valueProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (!Main.isSpeedSliding)
+                return;
+            ((TextField) Utils.getNodeByID("speedMultyTextfield")).setText(Float.parseFloat((new DecimalFormat("0.0"))
+                    .format(((Slider) Utils.getNodeByID("speedMultySlider")).getValue() / 100)) + ""); //round slider data and set it to speed field
+        });
     }
 
-    @FXML private void pauseFlight() {
-        if ( FlightStatus.getValue() == null || FlightStatus.getValue().equals("not Fly")) {
-            Alert a = new Alert(Alert.AlertType.ERROR);
-            a.setHeaderText("Error on pause flight");
-            a.setContentText("there is no flight to pause");
-            a.showAndWait();
-        } else
-            FlightStatus.set("pause Fly");
+    public void onMouseReleasedSpeedSlider() {
+        vm.change2();
+        Main.isSpeedSliding = false;
     }
 
-    @FXML public void SkipForward() {
-        forwardCnt.setValue(f++);
-        FlightStatus.set("skip");
-    }
-
-    @FXML public void SkipForwardTwice() {
-        forward2Cnt.setValue(f++);
-        FlightStatus.set("skip");
-    }
-
-    @FXML public void SkipBackward() {
-        backwardCnt.setValue(f++);
-        FlightStatus.set("skip");
-    }
-
-    @FXML public void SkipBackwardTwice() {
-        backward2Cnt.setValue(f++);
-        FlightStatus.set("skip");
-    }
 }
-
-
-//    @FXML private ImageView doubleSpeedDown;
-//    @FXML private ImageView slow;
-//    @FXML private ImageView play;
-//    @FXML private ImageView pause;
-//    @FXML private ImageView stop;
-//    @FXML private ImageView speed;
-//    @FXML private ImageView doubleSpeedUp;
-//
-//
-//    public Runnable onPlay, onPause, onStop;
-//
-//    @FXML public Pane IconsPane;
-//    @FXML public Label labelSpeed;
-//
-//
-//    public void play(){
-//        if(onPlay!=null)
-//            onPlay.run();
-//    }
-//    public void pause(){
-//        if(onPause!=null)
-//            onPause.run();
-//    }
-//    public void stop(){
-//        if(onStop!=null)
-//            onStop.run();
-//    }
-//  /*  public void playClicked(MouseEvent mouseEvent){ System.out.println("play the simulator"); }
-//    public void pauseClicked(MouseEvent mouseEvent){
-//        System.out.println("pause the simulator");
-//    }
-//    public void stopClicked(MouseEvent mouseEvent){
-//        System.out.println("stop the simulator");
-//    }*/
-//    public void speedDownClicked(MouseEvent mouseEvent){
-//        System.out.println("slow down the simulator");
-//    }
-//    public void speedUpClicked(MouseEvent mouseEvent){
-//        System.out.println("speed up the simulator");
-//    }
-//    public void doubleSpeedDownClicked(MouseEvent mouseEvent){
-//        System.out.println("slow down X2 the simulator");
-//    }
-//    public void doubleSpeedUpClicked(MouseEvent mouseEvent){
-//        System.out.println("speed up X2 the simulator");
-//    }
-//
-//    @Override
-//    public void initialize(URL location, ResourceBundle resources) {
-//        doubleSpeedDown.setImage(new Image("speed_down.jpg"));
-//        slow.setImage(new Image("slow.jpg"));
-//        play.setImage(new Image("play.jpg"));
-//        pause.setImage(new Image("pause.jpg"));
-//        stop.setImage(new Image("stop.jpg"));
-//        speed.setImage(new Image("speed.jpg"));
-//        doubleSpeedUp.setImage(new Image("speed_up.jpg"));
-//    }
-//
-//}
