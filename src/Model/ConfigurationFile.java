b6206884fdde6aad319f03fbbc588aecc093dae1
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-24 09:11:35 +0300

    Add config file

---
+++ src/Model/ConfigurationFile.java
@@ -0,0 +1,34 @@
+package Model;
+
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+//contain any global configurations for the simulator
+public class ConfigurationFile {
+
+	public String simulator_path;
+	public String simulator_playback;
+	public String flight_data_csv;
+	public float playback_speed_multiplayer;
+	public int playback_sample_rate_ms;
+	public int simulator_input_port;
+	public int simulator_output_port;
+	public int init_sleep_seconds;
+
+//	  Reads a ConfigurationFile from an XML using XMLDecoder
+	public static ConfigurationFile readConfigFromXML(String xmlPath) throws FileNotFoundException {
+		XMLDecoder decoder = new XMLDecoder(new FileInputStream(xmlPath));
+		return (ConfigurationFile) decoder.readObject();
+	}
+
+//	  Writes a ConfigurationFile to an XML using XMLEncoder
+	public static void saveConfigToXML(ConfigurationFile config, String xmlPath) throws FileNotFoundException {
+		XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(xmlPath)));
+		encoder.writeObject(config);
+		encoder.close();
+	}
+}
