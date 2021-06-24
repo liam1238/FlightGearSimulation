8990221a320b67fa2d867b48e13fdca709fab7e3
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-24 09:25:07 +0300

    Update ViewModel.java
    bindings and methods

--- src/ViewModel/ViewModel.java
+++ src/ViewModel/ViewModel.java
@@ -1,48 +1,110 @@
 package ViewModel;
 
-import Model.FlightGearAPI;
 import Model.Model;
-import Model.SimulatorAPI;
-import javafx.scene.control.Alert;
-import plugin.AnomalyDetectionAlgorithm;
+import View.Icons.IconsController;
+import View.View;
+import javafx.beans.property.*;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import Algorithms.AnomalyDetectionAlgorithm;
 
-import java.io.IOException;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 public class ViewModel extends Observable implements Observer {
-    Model model;
-   // public Runnable play,pause,stop, speedUp, speedDown, doubleSpeedUp, doubleSpeedDown;
+    public static Model m;
+    public static View view;
+    public static AnomalyDetectionAlgorithm algorithm;
+    public static IconsController ic;
 
-    public static ConfigurationFile conf;
-    public static SimulatorAPI simulatorApi;
-    public static AnomalyDetectionAlgorithm plugin;
+    public FloatProperty aileron = new SimpleFloatProperty(0);
+    public FloatProperty elevator = new SimpleFloatProperty(0);
+    public FloatProperty rudder = new SimpleFloatProperty(0);
+    public FloatProperty throttle = new SimpleFloatProperty(0);
+    public IntegerProperty currentFlightTime = new SimpleIntegerProperty(0);
+    public IntegerProperty speedMultySlider = new SimpleIntegerProperty(0);
+    public StringProperty yaw = new SimpleStringProperty("0");
+    public StringProperty roll = new SimpleStringProperty("0");
+    public StringProperty pitch = new SimpleStringProperty("0");
+    public StringProperty speed = new SimpleStringProperty("0");
+    public StringProperty direction = new SimpleStringProperty("0");
+    public StringProperty altitude = new SimpleStringProperty("0");
+    public StringProperty currentTimeString = new SimpleStringProperty("0");
+
+//    public SimpleDoubleProperty rate = new SimpleDoubleProperty();
+
     public ViewModel(Model m){
-        this.model = m;
+        ViewModel.m = m;
         m.addObserver(this);
     }
 
-    public void start(){
-        try {
-            conf = ConfigurationFile.readConfigFromXML("config.xml");
-            simulatorApi = new FlightGearAPI();
-            simulatorApi.start();
-        } catch (IOException | InterruptedException e1) {
-            new Alert(Alert.AlertType.ERROR, "Critical error: Could not read config / playback XML / Invalid simulator path in config").showAndWait();
+    public ViewModel() {
+        view = new View();
+        ic = view.ic;
+        m.addObserver(this);
+        ic.addObserver(this);
+    }
+
+    public void start() { m.readConfig(); }
+
+    public void startFlight(){ Model.simulatorApi.startToFly(); }
+
+    @Override
+    public void update(Observable o, Object arg) {
+//        System.out.println(o.toString());
+        if (o == view) {
+
+            if (arg.equals("play")) {
+                play();
+                setChanged();
+            }
+            if (arg.equals("pause")) {
+                pause();
+                setChanged();
+            }
+            if (arg.equals("stop")) {
+                stop();
+                setChanged();
+            }
+            if (arg.equals("slow")) {
+                speedDown();
+                setChanged();
+            }
+            if (arg.equals("fast")) {
+                speedUp();
+                setChanged();
+            }
+            if (arg.equals("slower")) {
+                doubleSpeedDown();
+                setChanged();
+            }
+            if (arg.equals("faster")) {
+                doubleSpeedUp();
+                setChanged();
+            }
         }
     }
 
-    @Override
-    public void update(Observable o, Object arg) { }
-
-    public void openCSV(){ model.openCSVFile(); }
-    public void play() {model.play(); setChanged(); notifyObservers();}
-    public void pause(){ model.pause(); setChanged(); notifyObservers();}
-    public void stop(){ model.stop(); setChanged(); notifyObservers();}
-    public void speedUp(){ model.speedUp();setChanged(); notifyObservers(); }
-    public void speedDown(){ model.speedDown(); setChanged(); notifyObservers();}
-    public void doubleSpeedUp(){ model.doubleSpeedUp(); setChanged(); notifyObservers();}
-    public void doubleSpeedDown(){ model.doubleSpeedDown(); setChanged(); notifyObservers();}
-    public void change(){ model.changeTextFiled(); }
-    public void change2(){ model.changeInSpeedSlider(); }
+    public void openCSV(){ m.openCSVFile(); }
+    public TextField getSpeedText(){return IconsController.SpeedText;}
+    public Slider getSpeedSlider(){return IconsController.SpeedSlider;}
+    public void bind(){ view.bind();}
+    public void play() {m.play(); }
+    public void pause(){ m.pause(); setChanged(); notifyObservers();}
+    public void stop(){ m.stop(); setChanged(); notifyObservers();}
+    public void speedUp(){ m.speedUp(); setChanged(); notifyObservers(); }
+    public void speedDown(){ m.speedDown(); setChanged(); notifyObservers();}
+    public void doubleSpeedUp(){ m.doubleSpeedUp(); setChanged(); notifyObservers();}
+    public void doubleSpeedDown(){ m.doubleSpeedDown(); setChanged(); notifyObservers();}
+    public void change(){ m.changeSpeedText(); }
+    public void change2(){ m.changeInSpeedSlider(); }
+    public int getFlightLength() { return Model.simulatorApi.getFlightLength(); }
+    public int getCurrentTime(){return Model.simulatorApi.getCurrentTime();}
+    public float getParameter(String s){ return Model.simulatorApi.getParameter(s); }
+    public String[] getFlightData(){return Model.simulatorApi.getData();}
+    public List<String> getDataList(){return Model.simulatorApi.getDataList();}
+    public int getFlightDataIndex(int MS){return Model.simulatorApi.getFlightDataIndexByMs(MS); }
+    public int getParameterIndex(String s){return Model.simulatorApi.getFlightParameterIndex(s);}
+    public void setCurrentFlightTime(int time){ Model.simulatorApi.setCurrentTime(time);}
 }
