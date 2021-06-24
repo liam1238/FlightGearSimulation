b063c5bb9857e7bc295ccefc15115aeede2eb97a
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-24 09:09:44 +0300

    Update Model.java

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,12 +1,10 @@
 package Model;
 
-import ViewModel.*;
+import View.Main;
+import ViewModel.ViewModel;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -15,18 +13,19 @@ import java.text.DecimalFormat;
 import java.util.Observable;
 
 public class Model extends Observable implements ModelInterface  {
-    //public Runnable play,pause,stop, speedUp, speedDown, doubleSpeedUp, doubleSpeedDown;
-    StringProperty CsvPath;
 
-    public Model(){
-        CsvPath = new SimpleStringProperty();
-    }
+    StringProperty CsvPath;
+    ViewModel vm;
+    public static ConfigurationFile config;
+    public static SimulatorAPI simulatorApi;
+
+    public Model(){ CsvPath = new SimpleStringProperty(); }
 
     @Override
     public void openCSVFile() {
         FileChooser fc = new FileChooser();
         fc.setTitle("CSV File Choose");
-        fc.setInitialDirectory(new File("./resources"));
+        fc.setInitialDirectory(new File("resources"));
         FileChooser.ExtensionFilter ef = new FileChooser.ExtensionFilter("CSV Files (*.csv)","*.csv");
         fc.getExtensionFilters().add(ef);
         File chosen = fc.showOpenDialog(null);
@@ -34,72 +33,68 @@ public class Model extends Observable implements ModelInterface  {
             new Alert(Alert.AlertType.ERROR, "Error: Invalid Selected CSV File, please try again").showAndWait();
             return;
         }
-        CsvPath.setValue("resources/"+chosen.getName());
+        CsvPath.setValue("resources/" +chosen.getName());
         CsvPath.setValue("anomaly_flight.csv/"+chosen.getName());
-        ViewModel.conf.flight_data_csv = chosen.getAbsolutePath();
-        ViewModel.conf.playback_speed_multiplayer = 0; //start on pause mode
+        config.flight_data_csv = chosen.getAbsolutePath();
+        config.playback_speed_multiplayer = 0; //start on pause mode
 
-        System.out.println("trying to start flying...");
-        try { //use API to send flight data
-            ViewModel.simulatorApi.loadFlightDataFromCSV(ViewModel.conf.flight_data_csv);
-            ViewModel.simulatorApi.sendFlightDataToSimulator();
-        } catch (IOException e) {
-            e.printStackTrace();
-            new Alert(Alert.AlertType.ERROR, "Error: Could not send flight data to the simulator").showAndWait();
-            return;
-        }
-        System.out.println("start to fly!");
-        Utils.setDisableALL(false); //enable all the icons after we started to fly
-        Utils.getNodeByID("openButton").setDisable(true); //disable open button after first use
-        ((Label)Utils.getNodeByID("totalFlightTimeLabel")).setText(Utils.msToTimeString(ViewModel.simulatorApi.getFlightLength())); //update total flight time label
-        ((Slider)Utils.getNodeByID("currentFlightTimeSlider")).setMax(ViewModel.simulatorApi.getFlightLength()); //update flight time slider
+        vm = Main.viewModel;
+        this.addObserver(vm);
     }
 
     @Override //play by setting speed to 1
-    public void play() { changeSpeedAndUpdateGUI(1); }
+    public void play() { changeFlightSpeed(1); }
 
     @Override //pause by setting speed to 0
-    public void pause() { changeSpeedAndUpdateGUI(0); }
+    public void pause() { changeFlightSpeed(0); }
 
     @Override  //pause and stop the flight. current time will be 0
-    public void stop() { pause(); ViewModel.simulatorApi.setCurrentFlightTime(0); }
+    public void stop() { pause(); simulatorApi.setCurrentTime(0); }
 
     @Override //increase speed
-    public void speedUp() { changeSpeedAndUpdateGUI(1.75f); }
+    public void speedUp() { changeFlightSpeed(1.75f); }
 
     @Override //decrease speed
-    public void speedDown() { changeSpeedAndUpdateGUI(0.5f); }
+    public void speedDown() { changeFlightSpeed(0.5f); }
 
     @Override //increase speed twice
-    public void doubleSpeedUp() { changeSpeedAndUpdateGUI(2f); }
+    public void doubleSpeedUp() { changeFlightSpeed(2f); }
 
     @Override //decrease speed twice
-    public void doubleSpeedDown() { changeSpeedAndUpdateGUI(0.25f); }
+    public void doubleSpeedDown() { changeFlightSpeed(0.25f); }
 
-    public void changeTextFiled(){
+    public void changeSpeedText(){
         try {
             //parse the text and update the simulation speed
-            float speedMulty = Float.parseFloat(((TextField) Utils.getNodeByID("speedMultyTextfield")).getText());
-            if (speedMulty < 0 || speedMulty > 5)
+            float speed = Float.parseFloat(vm.getSpeedText().getText());
+            if (speed < 0 || speed > 5)
                 throw new Exception("Invalid speed value");
-            changeSpeedAndUpdateGUI(speedMulty);
+            changeFlightSpeed(speed);
         } catch(Exception e) {
             e.printStackTrace();
             new Alert(Alert.AlertType.ERROR, "Error: Invalid speed multiplayer value: must be a number between 0 and 5").showAndWait();
-            changeSpeedAndUpdateGUI(1); } //change speed back to 1
+            changeFlightSpeed(1); } //change speed back to 1
     }
 
-    public void changeSpeedAndUpdateGUI(float speedMulty) {
-        speedMulty = Float.parseFloat((new DecimalFormat("0.00")).format(speedMulty));
-        ((TextField)Utils.getNodeByID("speedMultyTextfield")).setText(speedMulty + ""); //update speed text field
-        ((Slider)Utils.getNodeByID("speedMultySlider")).setValue((int)(speedMulty * 100)); //update speed slider
-        ViewModel.simulatorApi.setSimulationSpeed(speedMulty); //set simulation speed
+    public void changeFlightSpeed(float speed) {
+        speed = Float.parseFloat((new DecimalFormat("0.00")).format(speed));
+        vm.getSpeedText().setText(speed + ""); //update speed text field
+        vm.getSpeedSlider().setValue((int)(speed * 100)); //update speed slider
+        setChanged();
+        notifyObservers();
+        simulatorApi.setSimulationSpeed(speed); //set simulation speed
     }
 
-    public void changeInSpeedSlider(){
-        changeSpeedAndUpdateGUI(((float)(((Slider)Utils.getNodeByID("speedMultySlider")).getValue()))/100); //set flight speed
+    public void changeInSpeedSlider(){ changeFlightSpeed(((float)(vm.speedMultySlider.getValue()))/100); } //set flight speed
+
+    public void readConfig() {
+        try {
+            config = ConfigurationFile.readConfigFromXML("config.xml");
+            simulatorApi = new FlightGearAPI();
+            simulatorApi.start();
+        } catch (IOException | InterruptedException e1) {
+            e1.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Critical error: Could not read config / playback XML / Invalid simulator path in config").showAndWait();
+        }
     }
-
-
-
 }
