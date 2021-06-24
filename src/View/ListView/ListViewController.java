cb3a260dd9f3f255499463c76d782f2aae96e11d
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-24 09:13:26 +0300

    Update ListViewController.java

--- src/View/ListView/ListViewController.java
+++ src/View/ListView/ListViewController.java
@@ -1,20 +1,19 @@
 package View.ListView;
 
+import Model.Model;
+import View.Graphs.GraphsController;
 import View.Icons.IconsController;
+import View.Joystick.JoystickController;
 import View.Main;
-import ViewModel.*;
+import ViewModel.Service;
+import ViewModel.ViewModel;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.Slider;
 import javafx.scene.shape.Circle;
 
 import java.util.List;
@@ -22,26 +21,47 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class ListViewController {
-    ViewModel vm;
+
+    public static ViewModel vm;
 
     @FXML ListView listView;
     @FXML Button openButton;
+    @FXML ListView<String> ListView;
+    @FXML ListView<String> parametersList;
+
     StringProperty XmlPath;
-    IconsController ic;
+    public static IconsController ic;
+    public static JoystickController jc;
+    int flightLength;
 
     public ListViewController() {
         super();
+        vm = Main.viewModel;
         XmlPath = new SimpleStringProperty();
         ic = new IconsController();
-        vm = Main.viewModel;
+        jc = new JoystickController();
+    }
+
+        public void setListView(){
+        openButton = (Button) Service.getNodeByID("openButton");
+        parametersList = (ListView<String>) Service.getNodeByID("parametersList");
+        ListView = (ListView<String>) Service.getNodeByID("ListView");
     }
 
     public void onClickOpen() {
         vm.openCSV(); // call the model to choose a file and use it to display the flight over the flight gear
 
-        //set slider events
-        ic.onMouseMovedTimeSlider();
+        vm.startFlight();
+        flightLength = vm.getFlightLength();
+        Service.setDisableALL(false); //enable all the icons after we started to fly
+        openButton.setDisable(true); //disable open button after first use
+        IconsController.TotalFlightTime.setText(Service.msToTimeString(vm.getFlightLength())); //update total flight time label
+        IconsController.FlightSlider.setMax(vm.getFlightLength()); //update flight time slider
+
+       //ic.onMouseMovedTimeSlider();
         ic.onMouseMovedSpeedSlider();
+
+        vm.bind();
 
         //set current time updater
         Timer currentTimer = new Timer();
@@ -49,12 +69,12 @@ public class ListViewController {
             @Override
             public void run() {
                 Platform.runLater(() -> {  //update time slider as time goes on...
-                    if (Main.isTimeSliding)
+                    if (Main.SliderTimeTouch)
                         return;
-                    ((Slider)Utils.getNodeByID("currentFlightTimeSlider")).setValue(ViewModel.simulatorApi.getCurrentFlightTime());
-                    ((Label)Utils.getNodeByID("currentFlightTimeLabel")).setText(Utils.msToTimeString(ViewModel.simulatorApi.getCurrentFlightTime()));
+                    IconsController.FlightSlider.setValue(vm.getCurrentTime());
+                    IconsController.LabelCurrentTime.setText(Service.msToTimeString(vm.getCurrentTime()));
 
-                    if (ViewModel.simulatorApi.getCurrentFlightTime() >= ViewModel.simulatorApi.getFlightLength()) //when flight finished, reset the timers
+                    if (vm.getCurrentTime() >= flightLength) //when flight finished, reset the timers
                         ic.onClickStop();
                 });
             }
@@ -67,18 +87,18 @@ public class ListViewController {
             public void run() {
                 Platform.runLater(() -> {
                     //get flight parameter values and set them to joystick sliders
-                    float rudder = ViewModel.simulatorApi.getFlightParameter("rudder");
+                    float rudder = vm.getParameter("rudder");
                     if (rudder > -999)
-                        ((Slider)Utils.getNodeByID("rudderSlider")).setValue(rudder);
-                    float throttle = ViewModel.simulatorApi.getFlightParameter("throttle");
+                        JoystickController.rudderSlider.setValue(rudder);
+                    float throttle = vm.getParameter("throttle");
                     if (throttle > -999)
-                        ((Slider)Utils.getNodeByID("throttleSlider")).setValue(throttle);
+                       JoystickController.throttleSlider.setValue(throttle);
 
                     //get flight parameter values and set them to the joystick circle's offset
-                    float aileron = ViewModel.simulatorApi.getFlightParameter("aileron");
-                    float elevator = ViewModel.simulatorApi.getFlightParameter("elevator");
+                    float aileron = vm.getParameter("aileron");
+                    float elevator = vm.getParameter("elevator");
                     if (aileron > -999 && elevator > -999) {
-                        Circle joystick = (Circle)Utils.getNodeByID("joystickCircle");
+                        Circle joystick = JoystickController.movingCircle;
                         joystick.setTranslateX(aileron*150);
                         joystick.setTranslateY(elevator*-150);
                     }
@@ -86,22 +106,7 @@ public class ListViewController {
             }
         }, 0, 100);
 
-        //set dashboard panel updater
-        Timer dashboardTimer = new Timer();
-        dashboardTimer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                Platform.runLater(() -> {
-                    //get flight parameter values and set them to gauges
-                    ((Label)Utils.getNodeByID("altitudeLabel")).setText(ViewModel.simulatorApi.getFlightParameter("altimeter_indicated-altitude-ft")+"");
-                    ((Label)Utils.getNodeByID("airspeedLabel")).setText(ViewModel.simulatorApi.getFlightParameter("airspeed-indicator_indicated-speed-kt")+"");
-                    ((Label)Utils.getNodeByID("headingLabel")).setText(ViewModel.simulatorApi.getFlightParameter("indicated-heading-deg")+"");
-                    ((Label)Utils.getNodeByID("rollLabel")).setText(ViewModel.simulatorApi.getFlightParameter("attitude-indicator_indicated-roll-deg")+"");
-                    ((Label)Utils.getNodeByID("pitchLabel")).setText(ViewModel.simulatorApi.getFlightParameter("attitude-indicator_internal-pitch-deg")+"");
-                    ((Label)Utils.getNodeByID("yawLabel")).setText(ViewModel.simulatorApi.getFlightParameter("heading-deg")+"");
-                });
-            }
-        }, 0, 100);
+                jc.setDashboardPanel();
 
         //set graphs updater
         Timer graphTimer = new Timer();
@@ -109,59 +114,59 @@ public class ListViewController {
             @Override
             public void run() {
                 Platform.runLater(() -> {
-                    if (!Main.paramSelected) //don't run if graphs are uninitiated
+                    if (!Main.ChooseParameter) //don't run if graphs are uninitiated
                         return;
 
                     //erase graph data - so we can also support time jumps
-                    ((LineChart)Utils.getNodeByID("paramGraph1")).getData().clear();
-                    ((LineChart)Utils.getNodeByID("paramGraph2")).getData().clear();
-                    ((Canvas)Utils.getNodeByID("anomalyCanvas1")).getGraphicsContext2D().clearRect(0, 0, ((Canvas)Utils.getNodeByID("anomalyCanvas1"))
-                            .getWidth(), ((Canvas)Utils.getNodeByID("anomalyCanvas1")).getHeight());
+                    GraphsController.Graph1.getData().clear();
+                    GraphsController.Graph2.getData().clear();
+                    GraphsController.Canvas.getGraphicsContext2D().clearRect(0, 0, GraphsController.Canvas
+                            .getWidth(), GraphsController.Canvas.getHeight());
 
                     //create series for param graphs (start from 00:00:05 and move one second for each graph node)
                     XYChart.Series series = new XYChart.Series();
-                    for (int time = 5000; time < ViewModel.simulatorApi.getCurrentFlightTime(); time += 1000)
-                        try { series.getData().add(new XYChart.Data(time, Float.parseFloat(ViewModel.simulatorApi.getFlightData()[ViewModel.simulatorApi
-                                .getFlightDataIndexByMsTime(time)].split(",")[((ListView)Utils.getNodeByID("parameterListView")).getSelectionModel()
+                    for (int time = 5000; time < vm.getCurrentTime(); time += 1000)
+                        try { series.getData().add(new XYChart.Data(time, Float.parseFloat(vm.getFlightData()[vm.getFlightDataIndex(time)].split(",")[parametersList.getSelectionModel()
                                 .getSelectedIndex()]))); }
                     catch (Exception e) {e.printStackTrace(); }
 
-                    ((LineChart)Utils.getNodeByID("paramGraph1")).getData().add(series); //assign series
+                    GraphsController.Graph1.getData().add(series); //assign series
                     series = new XYChart.Series();
-                    for (int time = 5000; time < ViewModel.simulatorApi.getCurrentFlightTime(); time += 1000)
-                        try { series.getData().add(new XYChart.Data(time, Float.parseFloat(ViewModel.simulatorApi.getFlightData()[ViewModel.simulatorApi
-                                .getFlightDataIndexByMsTime(time)].split(",")[ViewModel.simulatorApi.getFlightParameterIndex((ViewModel.plugin
-                                .getCorrelated(((ListView)Utils.getNodeByID("parameterListView")).getSelectionModel().getSelectedItem().toString())))])));
+                    for (int time = 5000; time < Model.simulatorApi.getCurrentTime(); time += 1000)
+                        try { series.getData().add(new XYChart.Data(time, Float.parseFloat(vm.getFlightData()[vm.getFlightDataIndex(time)].split(",")[vm.getParameterIndex((ViewModel.algorithm
+                                .getCorrelated(parametersList.getSelectionModel().getSelectedItem())))])));
                         } catch (Exception e) {e.printStackTrace(); }
 
-                    ((LineChart)Utils.getNodeByID("paramGraph2")).getData().add(series); //assign series
+                    GraphsController.Graph2.getData().add(series); //assign series
 
-                    //draw anomaly graph on canvas via plugin
-                    ViewModel.plugin.drawOnGraph((Canvas)Utils.getNodeByID("anomalyCanvas1"), ((ListView)Utils.getNodeByID("parameterListView")).getSelectionModel()
-                            .getSelectedItem().toString(), ViewModel.simulatorApi.getFlightDataIndexByMsTime(ViewModel.simulatorApi.getCurrentFlightTime()));
+                    //draw anomaly graph on canvas via algorithm
+                    ViewModel.algorithm.drawOnGraph(GraphsController.Canvas, parametersList.getSelectionModel()
+                            .getSelectedItem(), vm.getFlightDataIndex(vm.getCurrentTime()));
                 });
             }
         }, 0, 100);
 
         //set lists-view values
         List<String> flightData;
-        flightData = ViewModel.simulatorApi.getFlightDataList();
-        ((ListView)Utils.getNodeByID("parameterListView")).getItems().addAll(flightData);
-        ((ListView)Utils.getNodeByID("classListView")).getItems().addAll(new String[] { "SimpleAnomalyDetector", "ZScoreAlgorithm", "HybridAlgorithm" });
-        ((ListView)Utils.getNodeByID("classListView")).getSelectionModel().select(0);
-        onMouseClickedClassListView(); //update detection plugin to default
+        flightData = vm.getDataList();
+        parametersList.getItems().addAll(flightData);
+        ListView.getItems().addAll("SimpleAnomalyDetector", "ZScoreAlgorithm", "HybridAlgorithm");
+        ListView.getSelectionModel().select(0);
+        onMouseClickedListView(); //update detection algorithm to default
+        ic.onMouseMovedTimeSlider();
     }
 
-    public void onMouseClickedClassListView() {
-        //update current plugin
-        Utils.loadPlugin(((ListView)Utils.getNodeByID("classListView")).getSelectionModel().getSelectedItem().toString());
+    public void onMouseClickedListView() {
+        //update current algorithm
+        Service.loadAlgorithm(ListView.getSelectionModel().getSelectedItem());
     }
 
-    public void onMouseClickedParameterListView() {
-        //update category axis label
-        Main.paramSelected = true;
-        ((NumberAxis)Utils.getNodeByID("paramCategoryAxis1")).setLabel((String)(((ListView)Utils.getNodeByID("parameterListView")).getSelectionModel().getSelectedItems().get(0)));
-        ((NumberAxis)Utils.getNodeByID("paramCategoryAxis2")).setLabel(ViewModel.plugin.getCorrelated(((ListView)Utils.getNodeByID("parameterListView")).getSelectionModel().getSelectedItem().toString()));
+    public void onMouseClickedParametersList() {
+        Main.ChooseParameter = true;
+        //gets parameter that we choose
+        GraphsController.chosenParameter.setLabel((parametersList.getSelectionModel().getSelectedItems().get(0)));
+        //gets the correlated parameter
+        GraphsController.correlatedParameter.setLabel(ViewModel.algorithm.getCorrelated(parametersList.getSelectionModel().getSelectedItem()));
     }
 
 }
