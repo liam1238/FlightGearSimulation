8f849f8de923669ccefadf1555328ff19a6bb14f
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-24 09:12:35 +0300

    Update Main.java

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,11 +1,10 @@
 package View;
 
 import Model.Model;
-import ViewModel.*;
+import ViewModel.ViewModel;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -16,9 +15,10 @@ public class Main extends Application {
 
 	public static ViewModel viewModel;
 	public static Scene scene;
-	public static boolean isTimeSliding = false;
-	public static boolean isSpeedSliding = false;
-	public static boolean paramSelected = false;
+	public static boolean SliderTimeTouch = false;
+	public static boolean SliderSpeedTouch = false;
+	public static boolean ChooseParameter = false;
+	public static View view;
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
@@ -29,20 +29,19 @@ public class Main extends Application {
 		viewModel.start();
 		System.out.println("Connected.");
 		BorderPane root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FlightSimulator.fxml")));
-		primaryStage.setTitle("Flight Simulator");
-		scene = new Scene(root,900,630);
+		scene = new Scene(root,900,600);
+		view = new View();
+		view.setAll();
 		primaryStage.setScene(scene);
+		primaryStage.setTitle("Flight Simulator");
 		primaryStage.setResizable(false);
 		primaryStage.show();
-		Utils.setDisableALL(true); //disable all icons before opening a csv file
-		((LineChart)Utils.getNodeByID("paramGraph1")).setCreateSymbols(false); //unable line charts from display dots
-		((LineChart)Utils.getNodeByID("paramGraph2")).setCreateSymbols(false); //unable line charts from display dots
 	}
 
 	@Override
 	public void stop(){
 		System.out.println("Stage is closing, bye bye...");
-	    try { ViewModel.simulatorApi.finalize(); } catch(Exception e) {e.printStackTrace();}; //finalize when we close the stage
+	    try { Model.simulatorApi.finalize(); } catch(Exception e) {e.printStackTrace();}; //finalize when we close the stage
 	}
 
 	public static void main(String[] args) {
