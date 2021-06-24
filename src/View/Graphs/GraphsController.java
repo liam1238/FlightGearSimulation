75f86fc4d835d5174c3a329c076ce684abf8300b
Liam Golan <74408724+liam1238@users.noreply.github.com>
2021-06-24 09:20:56 +0300

    Update GraphsController.java

--- src/View/Graphs/GraphsController.java
+++ src/View/Graphs/GraphsController.java
@@ -1,17 +1,29 @@
 package View.Graphs;
 
+import ViewModel.Service;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 
 public class GraphsController {
 
-    @FXML LineChart paramGraph1;
-    @FXML LineChart paramGraph2;
-    @FXML Canvas anomalyCanvas1;
+    @FXML public static NumberAxis chosenParameter;
+    @FXML public static NumberAxis correlatedParameter;
+    @FXML public static LineChart Graph1;
+    @FXML public static LineChart Graph2;
+    @FXML public static Canvas Canvas;
 
 
     public GraphsController() {
         super();
+    }
+
+    public void setGraph(){
+        Graph1 = (LineChart) Service.getNodeByID("Graph1");
+        Graph2 = (LineChart) Service.getNodeByID("Graph2");
+        Canvas = (Canvas) Service.getNodeByID("Canvas");
+        chosenParameter = (NumberAxis) Service.getNodeByID("chosenParameter");
+        correlatedParameter = (NumberAxis) Service.getNodeByID("correlatedParameter");
     }
 }
